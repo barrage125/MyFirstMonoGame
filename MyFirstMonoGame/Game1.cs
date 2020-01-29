895e9b9733b6ab859fd35aa9ef61ca12958be6b8
Alexander Lotz <alexandermlotz@gmail.com>
2020-01-29 14:05:04 -0500

    Testing and implementation or improved LERP-based movement and improvements on MonoGame keyboard class.

--- MyFirstMonoGame/Game1.cs
+++ MyFirstMonoGame/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
 
 namespace MyFirstMonoGame
 {
@@ -12,8 +14,12 @@ namespace MyFirstMonoGame
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         private Texture2D image;
-        private int playerPosX;
-        private int playerPosY;
+        private double playerPosX;
+        private double playerPosY;
+        private double playerTargetX;
+        private double playerTargetY;
+        private bool rightPress;
+        private int window;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -33,6 +39,8 @@ namespace MyFirstMonoGame
             base.Initialize();
             playerPosX = 384;
             playerPosY = 224;
+            playerTargetX = playerPosX;
+            playerTargetY = playerPosY;
         }
 
         /// <summary>
@@ -63,22 +71,62 @@ namespace MyFirstMonoGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Keyboard.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.IsPressed(Keys.Escape))
+            {
+                //Console.ReadLine();
                 Exit();
-
+            }
             // TODO: Add your update logic here
-            if (Keyboard.GetState().IsKeyDown(Keys.W)) {
-                playerPosY -= 2;
+            //if (Keyboard.GetState().IsKeyDown(Keys.W)) {
+            //    playerPosY -= 2;
+            //}
+            //if (Keyboard.GetState().IsKeyDown(Keys.A)) {
+            //    playerPosX -= 2;
+            //}
+            //if (Keyboard.GetState().IsKeyDown(Keys.S)) {
+            //    playerPosY += 2;
+            //}
+            //if (Keyboard.GetState().IsKeyDown(Keys.D)) {
+            //    playerPosX += 2;
+            //}
+
+            
+            if (Keyboard.IsPressed(Keys.W))
+            {
+                playerTargetY -= 5;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.A)) {
-                playerPosX -= 2;
+            if (Keyboard.IsPressed(Keys.A))
+            {
+                playerTargetX -= 5;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.S)) {
-                playerPosY += 2;
+            if (Keyboard.IsPressed(Keys.S))
+            {
+                playerTargetY += 5;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.D)) {
-                playerPosX += 2;
+            if (Keyboard.IsPressed(Keys.D))
+            {
+                //playerTargetX += 5;
+                //rightPress = true;
+                //window = 500;
             }
+            //Keyboard.GetState();
+            //Debug.WriteLine("Help!");
+            if (Keyboard.HasBeenPressed(Keys.D))
+            {
+                Debug.WriteLine("Help!");
+                playerTargetX += 20;
+                //rightPress = false;
+                //window = 0;
+            }
+
+            playerPosX += ((playerTargetX - playerPosX) * 0.1);
+            playerPosY += ((playerTargetY - playerPosY) * 0.1);
+
+            //if (window > 0)
+            //{
+            //    window--;
+            //}
             base.Update(gameTime);
         }
 
@@ -92,7 +140,7 @@ namespace MyFirstMonoGame
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(image, new Rectangle(playerPosX, playerPosY, 32, 32), Color.White);
+            spriteBatch.Draw(image, new Rectangle((int)playerPosX, (int)playerPosY, 32, 32), Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ MyFirstMonoGame/Keyboard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyFirstMonoGame
+{
+    class Keyboard
+    {
+        static KeyboardState currentKeyState;
+        static KeyboardState previousKeyState;
+        static string stringValue = string.Empty;
+
+        public static KeyboardState GetState()
+        {
+            previousKeyState = currentKeyState;
+            currentKeyState = Microsoft.Xna.Framework.Input.Keyboard.GetState();
+            Keys[] keys = currentKeyState.GetPressedKeys();
+
+            if (keys.Length > 0)
+            {
+                Debug.WriteLine(keys[0].ToString());
+                //var keyValue = keys[0].ToString();
+                //stringValue += keyValue;
+            }
+            //Debug.WriteLine(stringValue);
+            return currentKeyState;
+        }
+
+        public static bool IsPressed(Keys key)
+        {
+            return currentKeyState.IsKeyDown(key);
+        }
+
+        public static bool HasBeenPressed(Keys key)
+        {
+            if (currentKeyState.IsKeyDown(key))
+            { 
+                Debug.WriteLine("Now!");
+                if (!previousKeyState.IsKeyDown(key))
+                    Debug.WriteLine("Later!");
+            }
+            return (currentKeyState.IsKeyDown(key) && !(previousKeyState.IsKeyDown(key)));
+        }
+    }
+}
